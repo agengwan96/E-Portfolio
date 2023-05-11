84120689cbd158a59da0f915d33b160ae0fe9404
Ageng <agengwan@icloud.com>
2023-05-12 01:58:47 +0800

    added dark mode

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu__open');
 }
+
+let invert = false;
+
+function darkMode(){
+    if(invert==false){
+        invert = true;
+        document.body.classList += "invert";
+    }
+    else{
+        invert = false;
+        document.body.classList.remove("invert");
+    }
+}
